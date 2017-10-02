91a160f75cdd121bc16569dd119413834092c85b
sungkuk.kim <sungkuk5420@gmail.com>
2017-10-02 17:00:46 +0900

    translate.js -네이버 번역API를 사용한 번역기능 완성. -중복 번역 방지 추가. -양쪽언어 자동 탐지 추가 -번역 플래그 autoKey, checKey 추가 autoKey를 넣을경우 한글이냐 일본어냐 판단해서 번역하고  checKey를 넣으면 보낸 텍스트를 한글 영어로 자동 저장하지않고 2회에 걸쳐 좀더 정확한 번역결과를 저장한다.

--- public/javascripts/translate.js
+++ public/javascripts/translate.js
@@ -9,6 +9,17 @@ firebase.initializeApp(config);
 
 function getDataBase(cb){
     var DATABASE = firebase.database();
+    DATABASE.ref().on('child_added', function(data) {
+        console.log("추가!!!");
+        var database = data.val();
+        DBData = Object.keys(database).map(function(data) {
+            return {
+                id : data,
+                data :database[data]
+            };
+        });
+    });
+
     DATABASE.ref().on('value', function(data) {
         var database = data.val();
         DBData = Object.keys(database).map(function(data) {
@@ -21,6 +32,8 @@ function getDataBase(cb){
             cb(DBData);
         }
     });
+
+
 }
 function pushData(sendText,koText,jaText,translateTextObj){
     if(translateTextObj != undefined){
@@ -50,77 +63,106 @@ function pushData(sendText,koText,jaText,translateTextObj){
 }
 
 
+//checkKey를 넣으면 2번 검사한다. ex)한글 영어 따로따로 저장함. 안넣으면 입력키가 한글이면 한글에 바로 저장됨.
+//autoKey를 넣으면 언어를 자동으로 탐색해준다. 일본어를 넣어서 일본어 번역을 시도할경우 한국어가 나옴. 채팅의 경우 on해주면 좋음.
+function textTranslate(parmas){
 
-function allTextTranslate(resultLang){
+    //sendText, resultLang, checkKey, autoKey, cb
     //default lang is ja  ko convert to ja
     var sendLang = "ko",
-        returnLang = "ja";
+        resultLang = parmas.resultLang === "ko" ? "ko" : "ja",
+        checkKey  = parmas.checkKey === true ? true : false,
+        autoKey  = parmas.autoKey === true ? true : false;
 
 
     if (resultLang == "ko") {
         //ja convert to ko
-        sendLang = "ja",
-        returnLang = "ko";
+        sendLang = "ja";
+        resultLang = "ko";
     }
-    $('[data-langtext]').each(function() {
-        var $this = $(this);
-        var thisText = $.trim($this.text());
-        var translateTextObj = getTextData(thisText,resultLang);
 
-        if (resultLang == "ko") {
-            sendText = "";
-        }
+
+    if(parmas.sendText){
+        promiseFunc(parmas.sendText,function(translateTextObj){
+            if(parmas.cb){
+                parmas.cb(translateTextObj);
+            }
+        });
+    }else{
+        $('[data-langtext]').each(function() {
+            var $this = $(this);
+            var thisText = $.trim($this.text());
+            promiseFunc(thisText,function(translateTextObj){
+                var translateText = parmas.resultLang == "ko" ? translateTextObj.ko : translateTextObj.ja;
+                if(parmas.cb){
+                    parmas.cb(translateTextObj);
+                }
+                $this.text(translateText);
+            });
+
+        });
+    }
+
+    function promiseFunc(sendText,cb){
+        var translateTextObj = getTextData(sendText,resultLang);
 
         var bl = canTranslateApi(translateTextObj,resultLang);
 
 
         if(bl){
             // if(1){
-            var koText = thisText;
             var jaText ="";
-            var sendText = koText;
-            translateAPI(sendText,sendLang,returnLang,function(data){
+            var sendText = sendText;
+            translateAPI(sendText,sendLang,resultLang,function(data){
                 var translatedText = data.translatedText;
                 var baseLang = data.srcLangType;
 
-                if(sendText == translatedText){
+                if((sendText == translatedText)
+                    && (autoKey == true)){
 
                     //자동번역기능 만들기
-                    // if(canTranslateApi(getTextData(sendText,sendLang),returnLang)){
-                    //     translateAPI(sendText,returnLang,sendLang,function(data){
-                    //         var translatedText = data.translatedText;
-                    //         var baseLang = data.srcLangType;
-                    //         translateCB(translatedText,baseLang);
-                    //         $this.text(translatedText);
-                    //     });
-                    // }
+                    if(canTranslateApi(getTextData(sendText,sendLang),resultLang)){
+                        translateAPI(sendText,resultLang,sendLang,function(data){
+                            var translatedText = data.translatedText;
+                            translateCB(translatedText,resultLang,cb);
+                        });
+                    }
 
                     return false;
                 }
-                translateCB(translatedText,baseLang);
-                $this.text(translatedText);
+                translateCB(translatedText,resultLang,cb);
+
+
             });
 
-            function translateCB(translatedText,baseLang){
+            function translateCB(translatedText,resultLang,cb){
 
                 // console.log("sendText : "+ sendText + "translatedText : " + translatedText + "baseLang : " + baseLang);
-                if(baseLang == "ko"){
+                if(resultLang == "ja"){
                     jaText = translatedText;
-                    koText = "";
-                }else if(baseLang == "ja"){
-                    koText = translatedText;
-                    jaText = "";
+                    var koText = checkKey == true ? "" : sendText;
+                }else if(resultLang == "ko"){
+                    var koText = translatedText;
+                    jaText = checkKey == true ? "" : sendText;
                 }
                 // console.log("koText : " + koText + " jaText :" +jaText);
                 pushData(sendText,koText,jaText,translateTextObj);
-                $this.text(translatedText);
+                if(cb){
+                    cb({
+                        ko:koText,
+                        ja: jaText,
+                        resultLang : resultLang
+                    });
+                }
+
             }
         }else{
             console.log("이미 단어가 있습니다 : koText : " + translateTextObj.data.ko + " jaText :" + translateTextObj.data.ja);
-            var translateText = resultLang == "ko" ? translateTextObj.data.ko : translateTextObj.data.ja;
-            $this.text(translateText);
+            if(cb){
+                cb(translateTextObj.data);
+            }
         }
-    })
+    }
 
     function canTranslateApi(translateTextObj,resultLang){
 
@@ -144,7 +186,10 @@ function allTextTranslate(resultLang){
 
 
 }
-function translateAPI(sendText,sendLang,returnLang,cb){
+
+
+function translateAPI(sendText,sendLang,resultLang,cb){
+    console.log(sendText,sendLang,resultLang,cb);
     var apiUrl = '';
     $.ajax({
         type: 'GET',
@@ -152,7 +197,7 @@ function translateAPI(sendText,sendLang,returnLang,cb){
             request.setRequestHeader("content-type", 'text/javascript');
         },
         url: apiUrl+'/translate',
-        data : { text : sendText, sendLang : sendLang, returnLang : returnLang},
+        data : { text : sendText, sendLang : sendLang, resultLang : resultLang},
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function(data) {
@@ -192,6 +237,7 @@ function translateAPI(sendText,sendLang,returnLang,cb){
 
 function getTextData(sendText,resultLang){
     var textData = [];
+    console.log(DBData);
     if(resultLang == "ko"){
         textData = DBData.filter(function(item){
             return item.data.ja === sendText;
@@ -205,12 +251,22 @@ function getTextData(sendText,resultLang){
         return textData[0];
     }else{
         textData = DBData.filter(function(item){
-            return item.data.sendText === sendText;
+            return $.trim(item.data.sendText) === sendText;
         });
 
+        if(resultLang == "ko"){
+            textData = DBData.filter(function(item){
+                return $.trim(item.data.ko) === sendText;
+            });
+        }else if(resultLang == "ja"){
+            textData = DBData.filter(function(item){
+                return $.trim(item.data.ja) === sendText;
+            });
+        }
+
         if(textData.length !== 0) {
-            if ((textData[0].data.ko === "")
-                || (textData[0].data.ja === "")) {
+            if (((resultLang == "ko")&& (textData[0].data.ko === "") )
+                || ((resultLang == "ja")&& (textData[0].data.ja === ""))) {
                 textData = [];
             }
         }
@@ -226,6 +282,73 @@ function getTextData(sendText,resultLang){
 function translateInit(){
     getDataBase(function(){
         console.log(DBData);
+        addClickCss();
+        addPopupHTML();
         // allTextTranslate();
     });
+}
+
+function addClickCss(){
+    var cssHTML = "<style type='text/css'> ";
+    cssHTML += ".langClickCss:hover{ background-color:#ddd; font-weight:bold; curser: pointer;}";
+    cssHTML += " </style>";
+    $('head').append(cssHTML);
+    $('[data-langtext]').addClass('langClickCss');
+    $('.langClickCss').click(function(){
+        popupOpen($(this).text());
+    });
+}
+
+function addPopupHTML(){
+    var popupHTML = '<div id="mypopup" class="popup-ui"><div class="popup-ui-wrapper"><div class="popup-ui-content"><div class="my-content"><h3 class="textString"></h3><input class="inputForm"></div></div></div></div>"',
+        cssHTML = "<style type='text/css'> ",
+        inputCssHTML = "<style type='text/css'> ";
+
+    cssHTML += ".popup-ui { position: fixed; width: 100%; height: 100%; top: 0; left: 0; z-index: 7777; opacity: 0; visibility: hidden; background: rgba(0, 0, 0, 0.5); text-align: center; transition: all 0.2s ease-in-out; }";
+    cssHTML += ".popup-ui:before { content: ''; display: inline-block; height: 100%; vertical-align: middle; }";
+    cssHTML += ".popup-ui .popup-ui-wrapper { position: relative; display: inline-block; vertical-align: middle; margin: 0 auto; text-align: left; }";
+    cssHTML += ".popup-ui .popup-ui-wrapper .popup-ui-content { position: relative; background: #FFF; padding: 0; width: auto; margin: 0 auto; opacity: 0; visibility: hidden; border: solid 5px #FFF; max-width: 90%; transition: all 0.2s ease-in-out; transform: scale(0.8); box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.5); }";
+    cssHTML += ".popup-ui.show { opacity: 1; visibility: visible; }";
+    cssHTML += ".popup-ui.show .popup-ui-content { opacity: 1; visibility: visible; transform: scale(1); }";
+    cssHTML += ".my-content img { max-width: 100%; height: auto; }";
+    cssHTML += ".my-content h3 { padding: 0 10px; font-weight: 300; margin-bottom: 0; }";
+    cssHTML += ".my-content p { padding: 0 10px; font-size: 0.9em; }";
+    cssHTML += "#btnpopup { display: block; margin: 2em auto 0 auto; width: 200px; }";
+    cssHTML += " </style>";
+
+    cssHTML = "<style type='text/css'> ";
+
+    inputCssHTML += ".popup-ui { position: fixed; width: 100%; height: 100%; top: 0; left: 0; z-index: 7777; opacity: 0; visibility: hidden; background: rgba(0, 0, 0, 0.5); text-align: center; transition: all 0.2s ease-in-out; }";
+    inputCssHTML += ".popup-ui:before { content: ''; display: inline-block; height: 100%; vertical-align: middle; }";
+    inputCssHTML += ".popup-ui .popup-ui-wrapper { position: relative; display: inline-block; vertical-align: middle; margin: 0 auto; text-align: left; }";
+    inputCssHTML += ".popup-ui .popup-ui-wrapper .popup-ui-content { position: relative; background: #FFF; padding: 0; width: auto; margin: 0 auto; opacity: 0; visibility: hidden; border: solid 5px #FFF; max-width: 90%; transition: all 0.2s ease-in-out; transform: scale(0.8); box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.5); }";
+    inputCssHTML += ".popup-ui.show { opacity: 1; visibility: visible; }";
+    inputCssHTML += ".popup-ui.show .popup-ui-content { opacity: 1; visibility: visible; transform: scale(1); }";
+    inputCssHTML += ".my-content img { max-width: 100%; height: auto; }";
+    inputCssHTML += ".my-content h3 { padding: 0 10px; font-weight: 300; margin-bottom: 0; }";
+    inputCssHTML += ".my-content p { padding: 0 10px; font-size: 0.9em; }";
+    inputCssHTML += "#btnpopup { display: block; margin: 2em auto 0 auto; width: 200px; }";
+    inputCssHTML += " </style>";
+
+    $('body').append(popupHTML);
+    $('head').append(cssHTML);
+    $('head').append(inputCssHTML);
+
+    // js binding
+    var BUTTON = document.getElementById('btnpopup');
+    var POPUP = document.getElementById('mypopup');
+
+    // Popup modal (Click on modal to close it)
+    POPUP.addEventListener('click', function(){
+        if($(event.target).closest('.popup-ui-wrapper').length == 0){
+            this.className = 'popup-ui';
+        }
+    }, false);
+}
+function popupOpen(textString){
+    var POPUP = document.getElementById('mypopup');
+    POPUP.className = 'popup-ui  show';
+
+    $('#mypopup').find('.textString').text(textString);
+    $('#mypopup').find('.inputForm').attr('placeholder',textString);
 }
--- routes/index.js
+++ routes/index.js
@@ -11,25 +11,26 @@ var client_secret = '4_gCSpwtbz';
 router.get('/translate', function (req, res) {
     console.log(req.query.text);
     console.log(req.query.sendLang);
-    console.log(req.query.returnLang);
-    var api_url = 'https://openapi.naver.com/v1/language/translate';
+    console.log(req.query);
+    // var api_url = 'https://openapi.naver.com/v1/language/translate';  // papago smt
+    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';// papago nmt
     var request = require('request');
     var options = {
-      url: api_url,
-      form: {'source':req.query.sendLang, 'target':req.query.returnLang, 'text':req.query.text},
-    headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-  };
-  request.post(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+        url: api_url,
+        form: {'source':req.query.sendLang, 'target':req.query.resultLang, 'text':req.query.text},
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.post(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
 
-    // console.log(JSON.stringify(body));
-    //   res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-      res.end(JSON.stringify(body));
-    } else {
-      res.status(response.statusCode).end();
-      console.log('error = ' + response.statusCode);
-    }
-  });
+            // console.log(JSON.stringify(body));
+            //   res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+            res.end(JSON.stringify(body));
+        } else {
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+        }
+    });
 });
 
 module.exports = router;
