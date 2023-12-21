9a5c64014dc1e8f4a01fbacd858c3f8d84678ee9
kim sungkuk <sungkuk5420@gmail.com>
2023-12-21 18:12:05 +0900

    번역기능 오픈

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -27,30 +27,31 @@ router.get('/translateGoogle', function(req, res, next) {
 
 
 // 네이버 Papago NMT API 예제
-var client_id = 'vu8G4adE4oGZQHOrDH3C';
-var client_secret = '4_gCSpwtbz';
+var client_id = 'SaELp5LuZ87OYdG8LHVt';
+var client_secret = 'KOq42k_ftL';
 // var query = "とりあえず終わる見込みがあり作業工数が発生しているものはStage設定済み（見積もり対象月の設定）。下記レポートはStageなし（見積もり予定がまだ立っていないもの）の一覧ですので共有させていただきます。";
 router.get('/translate', function (req, res) {
     console.log(req.query.text);
     console.log(req.query.sendLang);
     console.log(req.query);
-    var api_url = 'https://openapi.naver.com/v1/language/translate';  // papago smt
-    // var api_url = 'https://openapi.naver.com/v1/papago/n2mt';// papago nmt
+    // var api_url = 'https://openapi.naver.com/v1/language/translate';  // papago smt
+    var api_url = 'https://openapi.naver.com/v1/papago/n2mt';// papago nmt
     var request = require('request');
     var options = {
         url: api_url,
         form: {'source':req.query.sendLang, 'target':req.query.resultLang, 'text':req.query.text},
-        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret,'Content-Type':'application/x-www-form-urlencoded'}
     };
     request.post(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
 
-            // console.log(JSON.stringify(body));
+            console.log(JSON.stringify(body.message));
             //   res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
             res.end(JSON.stringify(body));
         } else {
             res.status(response.statusCode).end();
             console.log('error = ' + response.statusCode);
+            console.log('error = ' + JSON.stringify(response));
         }
     });
 });
